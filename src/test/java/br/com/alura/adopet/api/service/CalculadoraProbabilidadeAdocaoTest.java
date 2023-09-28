d88a846c517c1b41d0615fadedef92d4070d6aba
Klayton Souza <klayton.al.souza@gmail.com>
2023-09-28 09:29:12 -0300

    Codigo 2: Teste referente a classe: CaculadoraProbabilidadeAdocao

--- src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
+++ src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
@@ -7,6 +7,7 @@ import br.com.alura.adopet.api.model.Pet;
 import br.com.alura.adopet.api.model.ProbabilidadeAdocao;
 import br.com.alura.adopet.api.model.TipoPet;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,7 +15,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculadoraProbabilidadeAdocaoTest {
 
     @Test
-    void cenario01(){
+    @DisplayName("Probabilidade alta para gatos jovens com peso baixo")
+    void probabilidadeAltaCenario01(){
         //idade 4 anos e 4kg - ALTA
 
         Abrigo abrigo = new Abrigo(new CadastroAbrigoDto(
@@ -37,7 +39,8 @@ class CalculadoraProbabilidadeAdocaoTest {
     }
 
     @Test
-    void cenario02(){
+    @DisplayName("Probabilidade media para gatos idosos com peso baixo")
+    void probabilidadeMediaCenario01(){
         //idade 15 anos e 4kg - MEDIA
 
         Abrigo abrigo = new Abrigo(new CadastroAbrigoDto(
