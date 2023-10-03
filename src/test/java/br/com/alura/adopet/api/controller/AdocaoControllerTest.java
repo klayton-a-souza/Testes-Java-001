ba1953fb5c6f0fd16f5e7993ff4e0a1c6e77540f
Klayton Souza <klayton.al.souza@gmail.com>
2023-10-03 20:15:01 -0300

    Codigo 5.4: Testando outros cenários

--- src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -1,9 +1,12 @@
 package br.com.alura.adopet.api.controller;
 
+import br.com.alura.adopet.api.service.AdocaoService;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -17,7 +20,11 @@ class AdocaoControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private AdocaoService service;
+
     @DisplayName("Deveria retornar o código 400 para solicitações de adoção com erros")
+    @Test
     void cenarioCodigo400() throws Exception {
         //ARRANGE
 
@@ -34,5 +41,33 @@ class AdocaoControllerTest {
         //ASSERT
 
         assertEquals(400, response.getStatus());
+    }
+
+
+    @DisplayName("Deveria retornar o código 200 para solicitações de adoção que não apresenta erro")
+    @Test
+    void cenarioCodigo200() throws Exception {
+        //ARRANGE
+
+        String json = """
+                {
+                    "idPet":1,
+                    "idTutor": 1,
+                    "motivo": "Motivo qualquer"               
+                }
+                """;
+
+
+        //ACT
+
+        var response = mvc.perform(
+                post("/adocoes")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //ASSERT
+
+        assertEquals(200, response.getStatus());
     }
 }
